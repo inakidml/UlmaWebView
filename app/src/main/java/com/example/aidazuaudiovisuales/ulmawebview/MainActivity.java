4a23bb122bfc262ae617e45b34052ef3ac7a8afe
Iñaki <inaki.diaz@ikasle.egibide.org>
2017-10-20 13:15:25 +0200

    proyecto copiado
    Enlace hard coded

---
+++ app/src/main/java/com/example/aidazuaudiovisuales/ulmawebview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.aidazuaudiovisuales.ulmawebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); // para ocultar la action bar
+        WebView web = (WebView)findViewById(R.id.web_view);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.setWebViewClient(new WebClient()); //para que no abra otro browser
+        web.loadUrl("https://ulma.com/ES/index.php");
+    }
+
+    private class WebClient extends WebViewClient {   //para que no se abra otro browser
+
+    }
+}
